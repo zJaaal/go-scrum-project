d76517252b2cd2e71829128eb1dacf613d95d140
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-16 12:39:02 -0300

    initial state of the app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        height: "inherit",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
