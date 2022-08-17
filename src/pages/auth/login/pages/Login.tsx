37fd2f0d51ea7c16dd1bde8415ee186d2bc8fa76
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-17 18:05:41 -0300

    fixed folders to maintain clean architecture

--- src/pages/auth/login/pages/Login.tsx
+++ src/pages/auth/login/pages/Login.tsx
@@ -1,6 +1,6 @@
 import LoginForm from "../components/Form";
 import "../../auth.styles.css";
-
+import "../../../../index.css";
 const LoginPage = () => {
   return (
     <div className="container">
--- src/pages/auth/register/pages/Register.tsx
+++ src/pages/auth/register/pages/Register.tsx
@@ -1,6 +1,6 @@
 import RegisterForm from "../components/Form";
 import "../../auth.styles.css";
-
+import "../../../../index.css";
 const RegisterPage = () => {
   return (
     <div className="container">
