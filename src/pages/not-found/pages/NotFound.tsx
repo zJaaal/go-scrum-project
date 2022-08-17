32471e484b141f77287633ad7a094cfdf16b6919
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-17 18:06:18 -0300

    Added Not found page

---
+++ src/pages/not-found/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../../index.css";
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <h1>404 Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
