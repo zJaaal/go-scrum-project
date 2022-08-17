f7dc9612ea5b7a4c77934927c280b10170d28fe6
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-17 18:06:28 -0300

    Added Tasks page

---
+++ src/pages/tasks/pages/Tasks.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../../index.css";
+const TasksPage = () => {
+  return (
+    <div className="container">
+      <h1>Tasks</h1>
+    </div>
+  );
+};
+
+export default TasksPage;
