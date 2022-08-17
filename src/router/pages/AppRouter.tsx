16fea94e73e4c9cbbac064b16ea1eb64a184c513
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-17 19:10:36 -0300

    Implemented Framer Motion

--- src/router/pages/AppRouter.tsx
+++ src/router/pages/AppRouter.tsx
@@ -1,18 +1,28 @@
+import { motion } from "framer-motion";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import AuthRoutes from "../components/AuthRoutes";
 import DashboardRoutes from "../components/DashboardRoutes";
 import PrivateRoutes from "../components/PrivateRoutes";
 import PublicRoutes from "../components/PublicRoutes";
+import { pageTransition } from "../utils";
 
 const AppRouter = () => {
+  const location = useLocation();
   return (
-    <Routes>
+    <Routes location={location} key={location.pathname}>
       <Route
         path="/*"
         element={
           <PrivateRoutes>
-            <DashboardRoutes />
+            <motion.div
+              initial="out"
+              animate="in"
+              exit="out"
+              variants={pageTransition}
+            >
+              <DashboardRoutes />
+            </motion.div>
           </PrivateRoutes>
         }
       />
@@ -20,7 +30,14 @@ const AppRouter = () => {
         path="/auth/*"
         element={
           <PublicRoutes>
-            <AuthRoutes />
+            <motion.div
+              initial="out"
+              animate="in"
+              exit="out"
+              variants={pageTransition}
+            >
+              <AuthRoutes />
+            </motion.div>
           </PublicRoutes>
         }
       />
