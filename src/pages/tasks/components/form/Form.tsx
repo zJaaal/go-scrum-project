2df2b951a58372c37ebefe54301b0f938deff2e5
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-18 18:34:20 -0300

    Added simple markup and functionality to Task Form

--- src/pages/tasks/components/form/Form.tsx
+++ src/pages/tasks/components/form/Form.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import { useFormik } from "formik";
+import { Task, TaskPriority, TaskType } from "../../types";
+import "./Form.styles.css";
+
+const initialValues: Task = {
+  id: Date.now(),
+  title: "",
+  datatime: Date.now().toString(),
+  creator: "Jalinson",
+  description: "",
+  type: TaskType.New,
+  priority: TaskPriority.Low,
+};
 
 const Form = () => {
-  return <section className="task-form">Form</section>;
+  const onSubmit = (values: Task) => {
+    alert(JSON.stringify(values));
+  };
+
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <section className="task-form">
+      <h2>Create Task</h2>
+      <p>Here you can create a task</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <input
+              type="text"
+              name="title"
+              onChange={handleChange}
+              value={values.title}
+              placeholder="Title"
+            />
+          </div>
+          <div>
+            <select
+              name="type"
+              id="type"
+              onChange={handleChange}
+              value={values.type}
+            >
+              <option value={""} disabled>
+                Status
+              </option>
+              {Object.values(TaskType).map((type, i) => (
+                <option value={type} key={i}>
+                  {type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <select
+              name="priority"
+              id="priority"
+              onChange={handleChange}
+              value={values.priority}
+            >
+              <option value={""} disabled>
+                Priority Level
+              </option>
+              {Object.values(TaskPriority).map((priority, i) => (
+                <option value={priority} key={i}>
+                  {priority}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          <textarea
+            name="description"
+            id="description"
+            onChange={handleChange}
+            value={values.description}
+          ></textarea>
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
