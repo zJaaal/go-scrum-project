43579914c1e833e8ef6581fd37019a62973f88ba
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-18 17:54:46 -0300

    Update useResize.ts

--- src/pages/tasks/hooks/useResize.ts
+++ src/pages/tasks/hooks/useResize.ts
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 
 const useResize = () => {
   const [isPhone, setIsPhone] = useState(window.innerWidth < 900);
+
   const resizeHandler = () => {
     setIsPhone(window.innerWidth < 900);
   };
+
   useEffect(() => {
     window.addEventListener("resize", resizeHandler);
 
