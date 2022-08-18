3bbc13b546d9d94db23e41ee05710805b75cbfd0
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-18 16:30:54 -0300

    Created CardProps type

--- src/pages/tasks/types/index.ts
+++ src/pages/tasks/types/index.ts
@@ -2,3 +2,13 @@ export type DescriptionState = {
   string: string;
   showMore: boolean;
 };
+
+export type CardProps = {
+  id: number;
+  title: string;
+  datatime: string;
+  creator: string;
+  description: string;
+  type: string;
+  priority: string;
+};
