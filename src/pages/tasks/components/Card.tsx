d364d3e5e47a1b49458c6adc9ba29c2d778ddc0b
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-18 16:31:04 -0300

    Created Card component

---
+++ src/pages/tasks/components/Card.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+import { CardProps } from "../types";
+
+const Card: FC<CardProps> = ({
+  title,
+  datatime,
+  creator,
+  description,
+  type,
+  priority,
+}) => {
+  return (
+    <div className="card">
+      <div className="close">x</div>
+      <h3>{title}</h3>
+      <h6>{datatime}</h6>
+      <h5>{creator}</h5>
+      <button type="button">{type}</button>
+      <button type="button">{priority}</button>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Card;
