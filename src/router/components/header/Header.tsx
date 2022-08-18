87c4a166d356f7114045ab3ea74a3c4a4894d045
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-18 16:05:49 -0300

    Create Header.tsx

---
+++ src/router/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import "./Header.styles.css";
+const Header = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("logged");
+    navigate("/auth", {
+      replace: true,
+    });
+  };
+  return (
+    <header>
+      <img
+        src="https://raw.githubusercontent.com/alkemyTech/Proyecto-GoScrum/unit05x01/public/img/goscrum.png"
+        alt="Logo"
+      />
+      <div onClick={handleLogout}>X</div>
+    </header>
+  );
+};
+
+export default Header;
