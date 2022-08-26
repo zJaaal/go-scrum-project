47ef02a289e483305171cc82bf6ebad37ef0b768
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-26 16:41:28 -0300

    Now you can post the tasks to your account

--- src/pages/tasks/components/form/Form.tsx
+++ src/pages/tasks/components/form/Form.tsx
@@ -1,29 +1,58 @@
 import { useFormik } from "formik";
-import { Task, TaskPriority, TaskType } from "../../types";
+import { Task, TaskImportance, TaskStatus } from "../../types";
 import { taskSchema } from "../../utils/validations/taskSchema";
 import "./Form.styles.css";
 
 const initialValues: Task = {
   id: Date.now(),
-  title: "",
   datatime: Date.now().toString(),
-  creator: "Jalinson",
+  creator: "",
+  title: "",
   description: "",
-  type: TaskType.New,
-  priority: TaskPriority.Low,
+  status: TaskStatus.New,
+  importance: TaskImportance.Low,
 };
 
 const Form = () => {
   const onSubmit = (values: Task) => {
-    alert(JSON.stringify(values));
+    fetch(`${import.meta.env.VITE_MAIN_URL}task/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        task: {
+          title: values.title,
+          description: values.description,
+          status: values.status,
+          importance: values.importance,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        resetForm();
+        alert(
+          "Task Created at: " +
+            new Date(res.result.task.createdAt).toLocaleDateString()
+        );
+      });
   };
 
-  const { handleSubmit, handleChange, handleBlur, values, errors, touched } =
-    useFormik({
-      initialValues,
-      validationSchema: taskSchema,
-      onSubmit,
-    });
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    touched,
+    resetForm,
+  } = useFormik({
+    initialValues,
+    validationSchema: taskSchema,
+    onSubmit,
+  });
 
   return (
     <section className="task-form">
@@ -47,24 +76,24 @@ const Form = () => {
           </div>
           <div>
             <select
-              name="type"
-              id="type"
+              name="status"
+              id="status"
               onChange={handleChange}
               onBlur={handleBlur}
-              value={values.type}
-              className={errors.type ? "error" : ""} //Im not really validating this, but ill let that for example
+              value={values.status}
+              className={errors.status ? "error" : ""} //Im not really validating this, but ill let that for example
             >
               <option value={""} disabled>
                 Status
               </option>
-              {Object.values(TaskType).map((type, i) => (
-                <option value={type} key={i}>
-                  {type}
+              {Object.values(TaskStatus).map((status, i) => (
+                <option value={status} key={i}>
+                  {status}
                 </option>
               ))}
             </select>
-            {errors.type && touched.type && (
-              <span className="error-message">{errors.type}</span>
+            {errors.status && touched.status && (
+              <span className="error-message">{errors.status}</span>
             )}
           </div>
           <div>
@@ -73,20 +102,20 @@ const Form = () => {
               id="priority"
               onChange={handleChange}
               onBlur={handleBlur}
-              value={values.priority}
-              className={errors.priority ? "error" : ""} //Im not really validating this, but ill let that for example
+              value={values.importance}
+              className={errors.importance ? "error" : ""} //Im not really validating this, but ill let that for example
             >
               <option value={""} disabled>
                 Priority Level
               </option>
-              {Object.values(TaskPriority).map((priority, i) => (
+              {Object.values(TaskImportance).map((priority, i) => (
                 <option value={priority} key={i}>
                   {priority}
                 </option>
               ))}
             </select>
-            {errors.priority && touched.priority && (
-              <span className="error-message">{errors.priority}</span>
+            {errors.importance && touched.importance && (
+              <span className="error-message">{errors.importance}</span>
             )}
           </div>
         </div>
@@ -94,9 +123,14 @@ const Form = () => {
           <textarea
             name="description"
             id="description"
+            onBlur={handleBlur}
             onChange={handleChange}
             value={values.description}
+            className={errors.description ? "error" : ""}
           ></textarea>
+          {errors.description && touched.description && (
+            <span className="error-message">{errors.description}</span>
+          )}
         </div>
 
         <button type="submit">Create</button>
